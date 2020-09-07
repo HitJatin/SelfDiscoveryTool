ccd39bc7980be280886e04eab9b362dc2c9a6015
Jatin <adriano23012001@gmail.com>
2020-09-07 17:30:08 +0530

    Replaced env with env.template

--- reactinterfacecode/src/index.js
+++ reactinterfacecode/src/index.js
@@ -32,17 +32,23 @@ fetch("./env.json")
 .then((response) => response.json())
 .then((data) =>
 {
-if (data!==undefined)
-config=data.config;
-ReactDOM.render(
-  <React.StrictMode>
-    <App initial={config}/>
-  </React.StrictMode>,
-  document.getElementById('root')
+  if (data!==undefined)
+  config=data.config;
+  ReactDOM.render(
+    <React.StrictMode>
+      <App initial={config}/>
+    </React.StrictMode>,
+    document.getElementById('root')
 )
 }
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-);
+).catch(function() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App initial={config}/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)});
 serviceWorker.unregister();
